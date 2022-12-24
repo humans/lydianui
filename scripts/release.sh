69a23e6765d509c9d54aeadfc8a19389448c9cc9
Jaggy <jaggy@humans.ph>
2022-12-24 14:28:06 +0800

    Add a release script with a verison bump

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-echo `pwd`
+VERSION=$1
+NAMESPACES=(packages/core packages/svelte)
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit
+fi
+
+for namespace in ${NAMESPACES[@]}; do
+  for directory in $namespace/*; do
+    package=$directory/package.json
+
+    if [ -f $package ]; then
+      echo "Bumping version for $directory to $VERSION"
+      jq ".version=\"$VERSION\"" $package > $directory/.new.package.json
+      mv $directory/.new.package.json $directory/package.json
+    fi
+  done
+done
+
 npm run compile -ws
