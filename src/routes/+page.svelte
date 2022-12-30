c776968fa4a236bc36dc1b45b35a6fc9194aefea
Jaggy <jaggy@humans.ph>
2022-12-30 10:54:22 +0800

    Completely remove npm workspaces for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
-	import * as Dialog from '@lydianui/svelte-dialog';
-	import { writable } from 'svelte/store';
-	// import { clickOutside } from 'packages/svelte/utilities';
-
 	export const prerender = true;
-
-	let open = true;
 </script>
 
 <svelte:head>
@@ -13,27 +7,3 @@
 </svelte:head>
 
 <h1>Lydian UI</h1>
-
-<!--<div class="[ bg-red-400 p-12 ]" use:clickOutside on:click-outside={() => console.error('outside')}>-->
-<!--	Hello-->
-<!--</div>-->
-
-<div>
-	Outside: {open}
-</div>
-
-<Dialog.Root bind:open let:dialog>
-	<Dialog.Panel on:click-outside={dialog.close}>
-		<Dialog.Title>This is a title</Dialog.Title>
-		<Dialog.Description>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium alias deleniti
-			dolores eaque est eum facilis inventore iusto laborum libero quibusdam, quidem quis quod sed
-			sunt tempore ut vero?
-		</Dialog.Description>
-		<Dialog.Description>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium alias deleniti
-			dolores eaque est eum facilis inventore iusto laborum libero quibusdam, quidem quis quod sed
-			sunt tempore ut vero?
-		</Dialog.Description>
-	</Dialog.Panel>
-</Dialog.Root>
