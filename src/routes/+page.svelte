dc00b95cb5dd541362363565352ae5c23d53bf56
Jaggy <jaggy@humans.ph>
2023-01-17 23:19:39 +0800

    Clean the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,5 @@
 <script>
 	import '../css/app.css';
-	import { NavigatableRoot, NavigatableItem } from '$lib';
-	import { readable } from 'svelte/store';
-
-	export const prerender = true;
-
-	const components = [
-		{ name: 'Cursor' },
-		{ name: 'Dialog' },
-		{ name: 'Dropdown Menu' },
-		{ name: 'Combobox' },
-		{ name: 'Listbox' },
-		{ name: 'Popover' }
-	];
 </script>
 
 <svelte:head>
@@ -21,28 +8,4 @@
 
 <main>
 	<h1>Lydian UI</h1>
-
-	<h2 id="navigation-list">Navigation List</h2>
-
-	<NavigatableRoot
-		items={readable(components)}
-		key="name"
-		aria-orientation="vertical"
-		role="listbox"
-		aria-labelledby="navigation-list"
-	>
-		{#each components as component}
-			<NavigatableItem
-				role="option"
-				key={component.name}
-				let:active
-				item={component}
-				on:select={() => alert(component.name)}
-			>
-				<div class:bg-red-700={active} class="[ py-24 ]">
-					{component.name}
-				</div>
-			</NavigatableItem>
-		{/each}
-	</NavigatableRoot>
 </main>
