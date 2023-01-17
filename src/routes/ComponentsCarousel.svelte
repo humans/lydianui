a1876707b2e892fb294b86a2bee4c66c3e8b89f5
Jaggy <jaggy@humans.ph>
2023-01-18 03:26:08 +0800

    CS fixes

--- src/routes/ComponentsCarousel.svelte
+++ src/routes/ComponentsCarousel.svelte
@@ -1,18 +1,16 @@
 <script>
 </script>
 
-<div class="carousel component-carousel [ overflow-x-scroll flex space-x-6 pb-12 ]">
+<div class="carousel component-carousel [ overflow-x-scroll flex space-x-6 pb-12 pr-24 ]">
 	{#each Array(5) as _}
-		<div>
+		<div class="carousel__item component">
 			<div
-				class="[ h-[22rem] w-[22rem] bg-neutral-100 rounded-xl flex items-center justify-center text-sm font-medium text-sm ]"
-			>
-				Example here
-			</div>
+				class="component__preview [ h-[22rem] w-[22rem] bg-neutral-100 rounded-xl flex items-center justify-center text-sm font-medium text-sm ]"
+			/>
 
 			<div class="[ leading-relaxed mt-3 pr-6 ]">
-				<h3 class="[ font-medium ]">Component Name</h3>
-				<p class="[ text-muted mt-0.5 ]">
+				<h3 class="component__name [ font-medium ]">Component Name</h3>
+				<p class="component__description [ text-muted mt-0.5 ]">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium at
 					deserunt doloribus,
 				</p>
@@ -24,8 +22,6 @@
 <style>
 	.component-carousel {
 		--container-padding: 8vw;
-		scroll-snap-align: start;
-		scroll-snap-stop: always;
 
 		padding-left: var(--container-padding);
 
