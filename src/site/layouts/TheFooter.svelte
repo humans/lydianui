68b0f17c1ab88e14e0c4251559cd3c0bcd960342
Jaggy <jaggy@humans.ph>
2023-01-18 12:09:20 +0800

    Add a logo at the footer

--- src/site/layouts/TheFooter.svelte
+++ src/site/layouts/TheFooter.svelte
@@ -1,5 +1,6 @@
 <script>
 	import ContentContainer from '@components/ContentContainer.svelte';
+	import Humans from '@icons/Humans.svelte';
 
 	export { className as class };
 
@@ -10,12 +11,10 @@
 	<ContentContainer size="max">
 		<div class="[ h-px bg-gradient-to-r from-muted/5 via-muted/60 to-muted/5$ ]" />
 
-		<div class="[ pt-12 flex flex-col items-center leading-relaxed space-y-1 ]">
+		<div class="[ pt-12 flex flex-col items-center leading-relaxed space-y-1.5 ]">
 			<a href="https://humans.ph" target="_blank">
-				<strong class="[ text-lg tracking-tight flex space-x-px ]">
-					<span class="[ font-bold ]">humans</span>
-					<span class="[ font-medium text-muted ]">ph</span>
-				</strong>
+				<span class="[ sr-only ]">Made by humans</span>
+				<Humans class="[ h-5 ]" />
 			</a>
 
 			<p class="[ text-sm text-muted ]">© 2023 Humans Philippines. All rights reserved.</p>
